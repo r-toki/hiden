e067d3833696b1a27033e71a8e2dc28a8aa06fa1
ReiSogawa <natto.and.eggs@gmail.com>
2020-11-28 02:27:45 +0900

    add: auth feature

---
+++ components/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-navbar>
+    <b-container>
+      <b-navbar-nav>
+        <template v-if="isLoggedIn">
+          <b-nav-item :to="{ name: 'index' }">ホーム</b-nav-item>
+          <b-nav-item :to="{ name: 'notes-new' }">新規作成</b-nav-item>
+          <b-nav-item @click="signOut">Sign Out</b-nav-item>
+        </template>
+        <b-nav-item v-else @click="signIn">Sign In</b-nav-item>
+      </b-navbar-nav>
+    </b-container>
+  </b-navbar>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  name: 'TheHeader',
+  computed: {
+    ...mapGetters('auth', ['isLoggedIn']),
+  },
+  methods: {
+    ...mapActions('auth', ['signIn', 'signOut']),
+  },
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,32 @@
 <template>
   <div>
-    <Nuxt />
+    <TheHeader />
+    <Nuxt v-if="isLoggedIn" />
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+import firebase, { usersRef } from '@/plugins/firebase'
+import { mapGetters, mapActions } from 'vuex'
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
+export default {
+  computed: {
+    ...mapGetters('auth', ['isLoggedIn']),
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(async (user) => {
+      if (user) {
+        const { uid, displayName } = user
+        await usersRef.doc(uid).set({ displayName })
+        await this.setCurrentUser({ id: uid, displayName })
+      } else {
+        await this.setCurrentUser(null)
+        this.$router.push('/')
+      }
+    })
+  },
+  methods: {
+    ...mapActions('auth', ['setCurrentUser']),
+  },
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+</script>
